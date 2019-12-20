dd6c7d1558700a0a31ee7b641ca4f2a906d4aa53
Awa Dieudonne Mbuh <dieudonneawa7@gmail.com>
2019-12-20 14:54:28 +0100

    leedcode two-sum solution

---
+++ Leedcode-solutions/two-sum.js
@@ -0,0 +1,20 @@
+// PROBLEM DESCRIPTION
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (var i = 0; i < nums.length - 1; i++) {
+        for (var j = i; j < nums.length; j++) {
+            if ((nums[i] + nums[j]) === target && i !== j) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+console.log(twoSum([1,3,4,2], 6));;
